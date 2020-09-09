90673ee0122f157eed12f089396b5a155324e2d1
andychen <andy.chen@iisigroup.com>
2020-09-09 22:22:15 +0800

    init

---
+++ src/main/java/com/andy/autocheckin/AutoCheckinApplication.java
@@ -0,0 +1,13 @@
+package com.andy.autocheckin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoCheckinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoCheckinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andy/autocheckin/AutoCheckinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andy.autocheckin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoCheckinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
