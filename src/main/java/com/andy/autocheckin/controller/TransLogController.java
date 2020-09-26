d5f43ad698968d2c26c2fbe38fa8b656c846a26c
andychen <andy.chen@iisigroup.com>
2020-09-26 21:04:44 +0800

    Add query log and error page

--- src/main/java/com/andy/autocheckin/controller/TransLogController.java
+++ src/main/java/com/andy/autocheckin/controller/TransLogController.java
@@ -2,6 +2,8 @@ package com.andy.autocheckin.controller;
 
 import com.andy.autocheckin.model.TransLog;
 import com.andy.autocheckin.service.TransLogService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +20,8 @@ import java.util.List;
 @Controller
 public class TransLogController {
 
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
     final private TransLogService transLogService;
 
     public TransLogController(TransLogService transLogService1) {
@@ -25,7 +29,10 @@ public class TransLogController {
     }
 
     @RequestMapping("/getTrans")
-    public String getTransDashboard(Model model) {
+    public String getTransDashboard(Model model, String ps) {
+        logger.info("Query trans log, psw: {}", ps);
+        if(!"cbu".equals(ps))
+            return "error";
         final List<TransLog> allLog = transLogService.getAllLog();
         model.addAttribute("trans", allLog);
         return "trans_board";
